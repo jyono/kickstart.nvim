ce74176b293cdeda500e420ed9665a3ed9729def
jordanyono <jordanyono@gmail.com>
2025-08-18 17:14:00 -0400

    lock and load

--- init.lua
+++ init.lua
@@ -349,7 +349,7 @@ require('lazy').setup({
     lazy = false,
     opts = {
       filesystem = {
-        visible = true,
+        visible = false,
         window = {
           mappings = {
             ['.'] = 'set_root', -- Set current directory as root
@@ -922,7 +922,7 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+        preset = 'super-tab',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
