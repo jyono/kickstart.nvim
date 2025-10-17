b44766ba67337aff75ca067ff58ff0b4619749ad
jordanyono <jordanyono@gmail.com>
2025-10-16 22:35:33 -0400

    yayaya

--- init.lua
+++ init.lua
@@ -824,6 +824,7 @@ require('lazy').setup({
           settings = {},
           on_attach = function(client)
             client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
           end,
         },
         --
