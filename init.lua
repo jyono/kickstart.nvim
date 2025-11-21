fea500cefbff2c8213aeef985b1476f2118bc8f9
jordanyono <jordanyono@gmail.com>
2025-11-21 16:50:22 -0500

    fix formatter

--- init.lua
+++ init.lua
@@ -989,17 +989,16 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        sql = { 'sql-formatter' },
+        sql = { 'sql_formatter' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        --formatters = {
       },
       formatters = {
-        ['sql-formatter'] = {
-          prepend_args = { '-l', 'postgresql' },
+        ['sql_formatter'] = {
+          prepend_args = { '-l', 'postgresql', '-c', '{"useTabs": true}' },
         },
       },
     },
